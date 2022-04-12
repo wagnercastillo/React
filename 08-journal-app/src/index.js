a95a20cb2d1b2bc7764312c25e65ed2c7e8f28c6
wagnercastillo <wagner.castillo@unl.edu.ec>
2022-04-12 13:01:36 -0500

    First Chance

---
+++ 08-journal-app/src/index.js
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import { JournalApp } from "./JournalApp";
+import "./styles/styles.scss";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<JournalApp />);
