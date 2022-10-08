1b341a54ef0309e1feadc77c0d66417867169950
WagnerCastillo <wagner.castillo@unl.edu.ec>
2022-10-07 19:34:35 -0500

    Primeros pasos en App Calendar

---
+++ 10-calendar/src/CalendarApp.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom"
+import { AppRouter } from "./router"
+
+export const CalendarApp = () => {
+  return (
+    <>
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+    </>
+  )
+}
