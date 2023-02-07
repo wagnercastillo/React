1e53ec9c9416bea4a0809f8aaaeb7450d4ef02c0
Wagner Castillo <wagner.castillo@unl.edu.ec>
2023-02-06 19:03:48 -0500

    Hash Router

--- 10-calendar/src/CalendarApp.jsx
+++ 10-calendar/src/CalendarApp.jsx
@@ -1,14 +1,16 @@
 import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, HashRouter } from "react-router-dom"
 import { AppRouter } from "./router"
 import { store } from "./store"
 
 export const CalendarApp = () => {
   return (
     <Provider store={ store }>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
+      {/* <BrowserRouter> */ }
+        <HashRouter>
+          <AppRouter />
+        </HashRouter>
+      {/* </BrowserRouter> */}
     </Provider>
   )
 }
